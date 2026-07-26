cd63e87bd6fa76ec274fb6ad2c850dd099825af7
abbad26 <abbad2606@gmail.com>
2026-07-27 00:43:40 +0530

    new commit

---
+++ src/maths/CountDigitsOfNumber.java
@@ -0,0 +1,23 @@
+package maths;
+
+import java.util.Scanner;
+
+public class CountDigitsOfNumber {
+   static int DigitCount(int num){
+        int count = 0;
+        if (num == 0) return 1;
+        while (num != 0){
+            num = num / 10;
+            count = count + 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int num = sc.nextInt();
+        int result =  DigitCount(num);
+        System.out.println(result);
+    }
+}
