0190f9698a2883a83ca270c86caca3c5913cb5d0
abbad26 <abbad2606@gmail.com>
2026-07-27 22:47:34 +0530

    second commit

--- src/maths/CountOddDigits.java
+++ src/maths/CountOddDigits.java
@@ -2,6 +2,12 @@ package maths;
 
 public class CountOddDigits {
 
+    public static void main(String[] args) {
+        //System.out.println(oddDigitCount(15));
+        System.out.println(oddDigitInRange(2,9));
+    }
+
+
     static int oddDigitCount(int num){
         int countOdd = 0;
 
@@ -13,7 +19,19 @@ public class CountOddDigits {
         }
         return  countOdd;
     }
-    public static void main(String[] args) {
-        System.out.println(oddDigitCount(15));
+
+    // low = 2 , high = 8 -> [2,3,4,5,6,7,8] -> number of odd digits = 3
+    static int oddDigitInRange(int low, int high){
+
+//        int oddCount = 0;
+//        for (int i = low; i <= high; i++){
+//
+//            if (i % 2 != 0) oddCount++;
+//        }
+//        return oddCount;
+
+        int nums = high - low + 1;  //  num=8-2+1 -> 7 / 2 = 3.5 -> 4
+        if (low % 2 != 0 && high % 2 != 0) return nums/2 + 1;
+        else return nums / 2;
     }
 }
