a403663867a38bd52bfec48ead1218c027588765
abbad26 <abbad2606@gmail.com>
2026-07-29 21:29:42 +0530

    first commit

--- src/maths/CountOddDigits.java
+++ src/maths/CountOddDigits.java
@@ -1,12 +1,19 @@
 package maths;
 
+import java.util.ArrayList;
+
 public class CountOddDigits {
 
     public static void main(String[] args) {
         //System.out.println(oddDigitCount(15));
        // System.out.println(oddDigitInRange(2,9));
        // System.out.println(reverseNumber(2584));
-        System.out.println(palindromeNumber(121));
+        //System.out.println(palindromeNumber(121));
+       // System.out.println(largestDigit(4765));
+       // System.out.println(factorial(6));
+       // System.out.println(isArmstrong(153));
+        //System.out.println(perfectNumber(12));
+
     }
 
 
@@ -56,4 +63,59 @@ public class CountOddDigits {
         int revNum = reverseNumber(num);
         return num == revNum;
     }
+
+    // largest digit in number
+    // num = 5867 -> largestDigit = 8, O(logn)
+    static int largestDigit(int num){
+        int largest = 0;
+
+        while (num > 0){
+            int lastDigit = num % 10;
+            if (lastDigit > largest) largest = lastDigit;
+
+            num = num / 10;
+        }
+        return largest;
+    }
+
+    // factorial of a number
+    // 4! -> 4*3*2*1 = 24
+    // 0! = 1
+    static int factorial(int num){
+        int fact = 1;
+        for(int i = 1; i <= num; i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    // Check if a number is Armstrong or not
+    // num = 153 then 1^3+5^3+3^3 = num -> 153 (true)
+
+    static boolean isArmstrong(int num){
+        int sum = 0;
+        int digitCount = (int) (Math.log10(num) + 1);
+        int duplicateNum = num;
+
+        while (num > 0){
+            int lastDigit = num % 10;
+            sum = (int) (sum + Math.pow(lastDigit, digitCount));
+
+            num = num / 10;
+        }
+        return duplicateNum == sum;
+    }
+
+    // Perfect Number
+    // num = 6 -> Proper Divisor -> 1,2,3 -> sumOfDivisor = 1+2+3 =num , so 6 is a Perfect Number
+    static boolean perfectNumber(int num){
+        int sum = 0;
+        for (int i = 1; i <= num/2; i++){
+            if (num % i == 0){
+                sum = sum + i;
+            }
+        }
+        return (sum == num);
+    }
+
 }
