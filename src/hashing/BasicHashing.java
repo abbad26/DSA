d30463a7e58b9384757ac08a95a3bc65962ad9eb
abbad26 <abbad2606@gmail.com>
2026-08-08 17:22:44 +0530

    first commit

--- src/hashing/BasicHashing.java
+++ src/hashing/BasicHashing.java
@@ -3,8 +3,9 @@ package hashing;
 public class BasicHashing {
     public static void main(String[] args) {
         int n = 7;
-        int[] arr = {3,5,2,1,5,3,7};
-        System.out.println(frequencyArray(arr, 5, n));
+        int[] arr = {5,4,3,1,5,3,7};
+       // System.out.println(frequencyArray(arr, 5, n));
+        System.out.println(mostFrequentElement(arr));
     }
 
 
@@ -26,4 +27,28 @@ public class BasicHashing {
     // T.C -> O(n^2)
     // to overcome this we can use array
 
+    static int mostFrequentElement(int[] nums){
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0 ; i < nums.length; i++ ){
+            max = Math.max(max, nums[i]);
+        }
+        // hash array
+        int[] hash = new int[max +1];
+        for (int i = 0 ; i < nums.length; i++){
+            hash[nums[i]]++; // store frequency
+        }
+        int mostFrequent = 0;
+        int maxFrequency = 0;
+        for (int i = 0; i <= max; i++) {
+            // to find the highest frequency
+            if (hash[i] > maxFrequency) {
+                maxFrequency = hash[i];
+                mostFrequent = i;
+            }
+        }
+        return mostFrequent;
+    }
+    // T.C -: o(n)+O(n)+O(m) -> O(n+m)
+    // S>C -: O(max)
 }
