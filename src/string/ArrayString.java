b945710828941b7b9b0afc79818610428710a650
abbad26 <abbad2606@gmail.com>
2026-08-13 23:48:42 +0530

    first

--- src/string/ArrayString.java
+++ src/string/ArrayString.java
@@ -1,20 +1,23 @@
 package string;
 
+import java.awt.datatransfer.StringSelection;
+
 public class ArrayString {
     public static void main(String[] args) {
         char[] s = {'a','b','b','a','d'};
-
-        String str = "racecar";
-
 //        reverseString(s);
 //        System.out.println(s);
 
-        palindromeString(str);
-        if (!palindromeString(str)){
-            System.out.println(str+ " is not a palindrome");
-        }else {
-            System.out.println(str+ " is a palindrome");
-        }
+        String str = "racecar";
+//        palindromeString(str);
+//        if (!palindromeString(str)){
+//            System.out.println(str+ " is not a palindrome");
+//        }else {
+//            System.out.println(str+ " is a palindrome");
+//        }
+
+        String strNum = "00546456";
+        System.out.println(largestOddNum(strNum));
     }
 
     // s = ['a','b','b','a','d'] -> abbad
@@ -37,7 +40,7 @@ public class ArrayString {
 
 
     // Palindrome
-
+    // s = "racecar"
     static boolean palindromeString(String s){
         int left = 0;
         int right = s.length() - 1;
@@ -50,5 +53,30 @@ public class ArrayString {
             right--;
         }
         return true;
+    }
+
+    // s = "0045876 -> to find odd last digit must be an odd digit
+    // oddNum -> 5, 45, 7,87,587,4587
+    // largest oddNum -> 4587
+
+    static String largestOddNum(String s){
+        int j = -1;
+        int n = s.length();
+
+        for (int i = n-1; i > 0; i--){
+            if ((s.charAt(i) - '0')%2 == 1) {
+                j = i;
+                break;
+            }
+        }
+
+        if (j == -1) return " ";
+
+        int i = 0;
+        while (i < n){
+            if (s.charAt(i) != '0') break;
+            i++;
+        }
+        return s.substring(i, j+1);
     }
 }
