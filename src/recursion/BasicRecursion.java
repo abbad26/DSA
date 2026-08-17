1608c16d3bc67b72d524a777eea3dee337b5b0a8
abbad26 <abbad2606@gmail.com>
2026-08-17 23:36:52 +0530

    reverse string

--- src/recursion/BasicRecursion.java
+++ src/recursion/BasicRecursion.java
@@ -1,13 +1,21 @@
 package recursion;
 
+import java.util.ArrayList;
+
 public class BasicRecursion {
     public static void main(String[] args) {
 
         int[] arr = {1,2,3,4,5};
+       // String s = "Hello";
+        ArrayList<Character> s = new ArrayList<>();
+        s.add('f');
+        s.add('l');
+        s.add('y');
        // System.out.println(sumOfNNumber(10));
        // System.out.println(factorialOfN(5));
        // System.out.println(getArraySum(arr));
-        System.out.println(fib(6));
+       // System.out.println(fib(6));
+        System.out.println(reverseString(s));
     }
 
 
@@ -36,11 +44,36 @@ public class BasicRecursion {
         return arr[index] + arraySum(arr, index + 1);
     }
 
+    // print nth fibonacci number
     // fibonacci = 0,1,1,2,3,5,8...
     // n = 3 , fib(3) -> fib(3-1)+fib(3-2) -> 1+1 = 2
     static int fib(int n){
         if (n <= 1) return n;
 
         return fib(n-1) + fib(n-2);
+    }
+
+    // reverse of string
+    // s = "hello" -> "olleh"
+
+    static ArrayList<Character> reverseString(ArrayList<Character> s){
+
+        int left = 0;
+        int right = s.size()-1;
+        reverseStr(s,left, right);
+        return s;
+    }
+
+    // to swap character
+    static private void reverseStr(ArrayList<Character> s, int left, int right){
+        // base
+        if (left >= right) return;
+
+        // swap character
+        char temp = s.get(left);
+        s.set(left, s.get(right));
+        s.set(right, temp);
+
+        reverseStr(s, left+1, right-1);
     }
 }
