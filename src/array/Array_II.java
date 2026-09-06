35000e0d2b5bf2d26f3a1507fea5bc32ccaef057
abbad26 <abbad2606@gmail.com>
2026-09-06 23:46:53 +0530

    two sum

--- src/array/Array_II.java
+++ src/array/Array_II.java
@@ -19,9 +19,6 @@ public class Array_II {
         //    System.out.println(Arrays.toString(unionOfArray(arr, arr1)));
         //   System.out.println(Arrays.toString(intersectionOfArray(arr, arr1)));
 
-        // System.out.println(majorityElement(arr));
-      //  System.out.println(leaders(arr));
-        System.out.println(Arrays.toString(rearrangeArrayBySign(arr)));
     }
 
     static void shiftZerosToEnd(int[] nums) {
@@ -224,91 +221,6 @@ public class Array_II {
         return intersection;
     }
 
-    static int majorityElement(int[] nums) {
-        int n = nums.length;
 
-//        for (int i = 0; i < n; i++) {
-//            int count = 0;
-//
-//            for (int j = 0; j < n; j++) {
-//                if (nums[i] == nums[j]) {
-//                    count++;
-//                }
-//            }
-//
-//            if (count > n / 2) {
-//                return nums[i];
-//            }
-//        }
-//
-//        return -1;
-        // O(n^2) -> t.c
-
-        // using hashmap
-
-        HashMap<Integer, Integer> elementFreq = new HashMap<>();
-
-        for (int i : nums) {
-            elementFreq.put(i, elementFreq.getOrDefault(i, 0) + 1);
-        }
-
-        for (Map.Entry<Integer, Integer> map : elementFreq.entrySet()) {
-            if (map.getValue() > n / 2) {
-                return map.getKey();
-            }
-        }
-
-        return -1;
-    }
-
-    // leaders in an array
-    //Input: nums = [1, 2, 5, 3, 1, 2]
-    //Output: [5, 3, 2] -> rightmost always a leader & element > all elements in its right
-
-    static List<Integer> leaders(int[] nums) {
-        int n = nums.length;
-         int left = n - 2;
-         int right = n - 1;
-        List<Integer> leaderList = new ArrayList<>();
-        leaderList.add(nums[n - 1]);
-        while (left < right && left >= 0){
-
-            if ( leaderList.get(leaderList.size() - 1) < nums[left]){
-                leaderList.add(nums[left]);
-            }
-                left--;
-                right--;
-
-        }
-        Collections.reverse(leaderList);
-        return leaderList;
-    }
-
-    //Rearrange array elements by sign
-    //Input : nums = [2, 4, 5, -1, -3, -4]
-    //Output : [2, -1, 4, -3, 5, -4] , 1st element always positive
-
-    static int[] rearrangeArrayBySign(int[] nums){
-         int n = nums.length;
-        List<Integer> positiveList = new ArrayList<>();
-        List<Integer> negativeList = new ArrayList<>();
-
-        // separate positive and negative array
-        for (int i = 0; i < n; i++){
-            if (nums[i] > 0){
-                positiveList.add(nums[i]);
-            }else {
-                negativeList.add(nums[i]);
-            }
-        }
-
-        // positive at even and negative at odd
-        for (int i = 0; i < n / 2; i++){
-            nums[2*i] = positiveList.get(i);
-            nums[2*i + 1] = negativeList.get(i);
-        }
-
-        return nums;
-    }
 }
 
---
+++ src/array/Array_III.java
@@ -0,0 +1,120 @@
+package array;
+
+import java.util.*;
+
+public class Array_III {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,5};
+        // System.out.println(majorityElement(arr));
+        //  System.out.println(leaders(arr));
+      //  System.out.println(Arrays.toString(rearrangeArrayBySign(arr)));
+        System.out.println(Arrays.toString(twoSum(arr, 4)));
+    }
+
+
+    static int majorityElement(int[] nums) {
+        int n = nums.length;
+
+//        for (int i = 0; i < n; i++) {
+//            int count = 0;
+//
+//            for (int j = 0; j < n; j++) {
+//                if (nums[i] == nums[j]) {
+//                    count++;
+//                }
+//            }
+//
+//            if (count > n / 2) {
+//                return nums[i];
+//            }
+//        }
+//
+//        return -1;
+        // O(n^2) -> t.c
+
+        // using hashmap
+
+        HashMap<Integer, Integer> elementFreq = new HashMap<>();
+
+        for (int i : nums) {
+            elementFreq.put(i, elementFreq.getOrDefault(i, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> map : elementFreq.entrySet()) {
+            if (map.getValue() > n / 2) {
+                return map.getKey();
+            }
+        }
+
+        return -1;
+    }
+
+    // leaders in an array
+    //Input: nums = [1, 2, 5, 3, 1, 2]
+    //Output: [5, 3, 2] -> rightmost always a leader & element > all elements in its right
+
+    static List<Integer> leaders(int[] nums) {
+        int n = nums.length;
+        int left = n - 2;
+        int right = n - 1;
+        List<Integer> leaderList = new ArrayList<>();
+        leaderList.add(nums[n - 1]);
+        while (left < right && left >= 0){
+
+            if ( leaderList.get(leaderList.size() - 1) < nums[left]){
+                leaderList.add(nums[left]);
+            }
+            left--;
+            right--;
+
+        }
+        Collections.reverse(leaderList);
+        return leaderList;
+    }
+
+    //Rearrange array elements by sign
+    //Input : nums = [2, 4, 5, -1, -3, -4]
+    //Output : [2, -1, 4, -3, 5, -4] , 1st element always positive
+
+    static int[] rearrangeArrayBySign(int[] nums){
+        int n = nums.length;
+        List<Integer> positiveList = new ArrayList<>();
+        List<Integer> negativeList = new ArrayList<>();
+
+        // separate positive and negative array
+        for (int i = 0; i < n; i++){
+            if (nums[i] > 0){
+                positiveList.add(nums[i]);
+            }else {
+                negativeList.add(nums[i]);
+            }
+        }
+
+        // positive at even and negative at odd
+        for (int i = 0; i < n / 2; i++){
+            nums[2*i] = positiveList.get(i);
+            nums[2*i + 1] = negativeList.get(i);
+        }
+
+        return nums;
+    }
+
+    // Two sum
+    // Input: nums = [1, 6, 2, 10, 3], target = 7
+    //Output: [0, 1]
+    static int[] twoSum(int[] nums, int target){
+
+        // Check every possible pair
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
